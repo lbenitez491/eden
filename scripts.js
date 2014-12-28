4dcd0c7fb84be3eec6e5f30845238abc41c19ca9
Luis Benítez <contacto@lbenitez.net>
2014-12-28 01:46:55 -0300

    Mejoras en el formulario de la especie
    Se agregó botones para pasar a la siguiente categoría, css, y funciones
    javascript.

--- scripts.js
+++ scripts.js
@@ -1,34 +1,84 @@
-<script>
-	$( "#nav-1 ul #li-1" ).click(function(event) {
-		event.preventDefault();
-		$( "#nav-1 ul #li-1" ).attr("class","li-select");
-		$( "#nav-1 ul #li-2" ).attr("class","");
-	  	$( "#nav-1 ul #li-3" ).attr("class","");
+/* Default */
+/* Elementos de la lista del formulario */
+$( "#nav-1 ul #li-1" ).click(function(event) {
+	event.preventDefault();
+	$( "#nav-1 ul li" ).attr("class","");
+	$( "#li-1" ).attr("class","li-select");
 
-	  	$( "#nav-1 ul #li-1 #a-1" ).attr("class","a-select");
-	  	$( "#nav-1 ul #li-2 #a-2" ).attr("class","");
-	  	$( "#nav-1 ul #li-3 #a-3" ).attr("class","");
-	});
+	$( "#nav-1 ul li a" ).attr("class","");
+	$( "#nav-1 ul li #a-1" ).attr("class","a-select");
 
-	$( "#nav-1 ul #li-2" ).click(function(event) {
-		event.preventDefault();
-	  	$( "#li-1" ).attr("class","");
-		$( "#li-2" ).attr("class","li-select");
-	  	$( "#li-3" ).attr("class","");
+	$( "#div-2 form #div-taxonomia" ).show();
+	$( "#div-2 form #div-4" ).hide();
+	$( "#div-2 form #div-5" ).hide();
+});
 
-	  	$( "#nav-1 ul #li-1 #a-1" ).attr("class","");
-	  	$( "#nav-1 ul #li-2 #a-2" ).attr("class","a-select");
-	  	$( "#nav-1 ul #li-3 #a-3" ).attr("class","");
-	});
+$( "#nav-1 ul #li-2" ).click(function(event) {
+	event.preventDefault();
+  	$( "#nav-1 ul li" ).attr("class","");
+	$( "#li-2" ).attr("class","li-select");
 
-	$( "#nav-1 ul #li-3" ).click(function(event) {
-		event.preventDefault();
-	  	$( "#li-1" ).attr("class","");
-		$( "#li-2" ).attr("class","");
-	  	$( "#li-3" ).attr("class","li-select");
+  	$( "#nav-1 ul li a" ).attr("class","");
+  	$( "#nav-1 ul li #a-2" ).attr("class","a-select");
 
-	  	$( "#nav-1 ul #li-1 #a-1" ).attr("class","");
-	  	$( "#nav-1 ul #li-2 #a-2" ).attr("class","");
-	  	$( "#nav-1 ul #li-3 #a-3" ).attr("class","a-select");
-	});
-</script>
+  	$( "#div-2 form #div-taxonomia" ).hide();
+  	$( "#div-2 form #div-4" ).show();
+  	$( "#div-2 form #div-5" ).hide();
+});
+
+$( "#nav-1 ul #li-3" ).click(function(event) {
+	event.preventDefault();
+  	$( "#nav-1 ul li" ).attr("class","");
+	$( "#li-3" ).attr("class","li-select");
+
+  	$( "#nav-1 ul li a" ).attr("class","");
+  	$( "#nav-1 ul li #a-3" ).attr("class","a-select");
+
+  	$( "#div-2 form #div-taxonomia" ).hide();
+  	$( "#div-2 form #div-4" ).hide();
+  	$( "#div-2 form #div-5" ).show();
+});
+
+/* Condición if de origen fitogeográfico */
+$( "#input4" ).click(function(event) {
+	event.preventDefault();
+  	if (this.value == "Nativo" || this.value == "Endémico" || this.value == "Cosmopolito") {
+  		$("#label-1").html("Regiones");
+  	}
+  	else if(this.value == "Introducido"){
+  		$("#label-1").html("Lugar de origen");
+  	};
+});
+
+$( "#boton-true-taxonomia" ).click(function(event) {
+  event.preventDefault();
+  $( "#nav-1 ul li" ).attr("class","");
+  $( "#li-2" ).attr("class","li-select");
+
+  $( "#nav-1 ul li a" ).attr("class","");
+  $( "#nav-1 ul li #a-2" ).attr("class","a-select");
+
+  $( "#div-2 form #div-taxonomia" ).hide();
+  $( "#div-2 form #div-4" ).show();
+  $( "#div-2 form #div-5" ).hide();
+});
+
+$( "#boton-true-descripcion" ).click(function(event) {
+  event.preventDefault();
+  $( "#nav-1 ul li" ).attr("class","");
+  $( "#li-3" ).attr("class","li-select");
+
+  $( "#nav-1 ul li a" ).attr("class","");
+  $( "#nav-1 ul li #a-3" ).attr("class","a-select");
+
+  $( "#div-2 form #div-taxonomia" ).hide();
+  $( "#div-2 form #div-4" ).hide();
+  $( "#div-2 form #div-5" ).show();
+});
+
+$( "#boton-true-distribucion" ).click(function(event) {
+  event.preventDefault();
+ 
+});
+
+
